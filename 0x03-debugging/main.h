4bda9ed57ffcbc8a77a825996584c8b86bf161e4
OnyangoFaith <117824136+OnyangoFaith@users.noreply.github.com>
2022-12-16 01:22:28 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,24 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
 #include <stdio.h>
 
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
 
+int main(void)
+{
+	int i;
 
-void positive_or_negative(int i);
+	printf("Infinite loop incoming :(\n");
 
+	i = 0;
 
-
-#endif /* MAIN_H */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
