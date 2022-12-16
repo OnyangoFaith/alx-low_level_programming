1928352c4a489e4f6b3c1b2cd1ae71576ed7bc15
OnyangoFaith <117824136+OnyangoFaith@users.noreply.github.com>
2022-12-16 05:12:47 +0300

    Add files via upload

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,20 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
-/**
- * main - causes an infinite loop
- * Return: 0
- */
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int, int);
+void print_remaining_days(int, int, int);
 
-int main(void)
-{
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-
-	/*
-	 */
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
-}
+#endif /* MAIN_H */
