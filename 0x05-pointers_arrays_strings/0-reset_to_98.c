32bcb9194dc071b9b73db2b781bf1921a2b96a06
OnyangoFaith <117824136+OnyangoFaith@users.noreply.github.com>
2022-12-20 23:25:48 +0300

    Add files via upload

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,11 @@
 #include "main.h"
-
 /**
- *
- *  * reset_to_98 - resets the value of given integer to 98
- *
- *   * @n: parameter
- *
- *    * Return: Nothing
- *
- *     */
-
-
+ * reset_to_98 - resets the value of given integer to 98
+ * @n: parameter
+ * Return: Nothing
+ */
 
 void reset_to_98(int *n)
-
 {
-
-		*n = 98;
-
+	*n = 98;
 }
