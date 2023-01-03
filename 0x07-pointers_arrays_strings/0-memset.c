accdbee0f33f6fe337e2cd00e253d050d5ec90a6
OnyangoFaith <imanijowan@gmail.com>
2023-01-03 09:51:42 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _memset - fills memory with constant byte
+ *
+ *   * @s: string to be filled
+ *
+ *    * @b: constant used to fill in the buffer
+ *
+ *     * @n: number of bytes
+ *
+ *      *
+ *
+ *       * Return: pointer to s
+ *
+ *        */
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+		unsigned int i;
+
+
+
+			for (i = 0; i < n; i++)
+
+					{
+
+								s[i] = b;
+
+									}
+
+
+
+				return (s);
+
+}
