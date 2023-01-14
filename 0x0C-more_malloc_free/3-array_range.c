d8385589e7f32554ec45305aa05c3013f94a4824
OnyangoFaith <117824136+OnyangoFaith@users.noreply.github.com>
2023-01-14 06:35:02 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,65 +1,33 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 /**
+ *array_range - creates an array of integers
  *
- *  *array_range - creates an array of integers
+ *@min: minumum value
+ *@max: maximum value
  *
- *   *
- *
- *    *@min: minumum value
- *
- *     *@max: maximum value
- *
- *      *
- *
- *       *Return: a pointer to the array or NULL if the process fails
- *
- *        */
-
+ *Return: a pointer to the array or NULL if the process fails
+ */
 int *array_range(int min, int max)
-
 {
+	int *array;
+	int amount = 0, i;
 
-		int *array;
-
-			int amount = 0, i;
-
-
-
-				if (min > max)
-
-						{
-
-									return (NULL);
-
-										}
-
-					amount = max - min + 1;
-
-						array = malloc(sizeof(int) * amount);
-
-							if (array == NULL)
-
-									{
-
-												return (NULL);
-
-													}
-
-								for (i = 0; i < amount; i++)
-
-										{
-
-													array[i] = min + i;
-
-														}
-
-									return (array);
-
+	if (min > max)
+	{
+		return (NULL);
+	}
+	amount = max - min + 1;
+	array = malloc(sizeof(int) * amount);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < amount; i++)
+	{
+		array[i] = min + i;
+	}
+	return (array);
 }
