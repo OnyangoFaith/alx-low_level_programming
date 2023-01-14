63a28177482ffb0c62cef8f72b6236f8c5d12c17
OnyangoFaith <117824136+OnyangoFaith@users.noreply.github.com>
2023-01-14 06:40:32 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,95 +1,48 @@
 #include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 /**
+ *_memset - fills memory with a constant byte n amount of times
  *
- *  *_memset - fills memory with a constant byte n amount of times
+ *@s: pointer that has the address of the memory to fill
+ *@b: constant byte that will be written into the momory
+ *@n: amount of times the byte will be written into the memory
  *
- *   *
- *
- *    *@s: pointer that has the address of the memory to fill
- *
- *     *@b: constant byte that will be written into the momory
- *
- *      *@n: amount of times the byte will be written into the memory
- *
- *       *
- *
- *        *Return: address of the memory where thebyte was written
- *
- *         */
-
+ *Return: address of the memory where thebyte was written
+ */
 char *_memset(char *s, char b, unsigned int n)
-
 {
+	unsigned int i;
 
-		unsigned int i;
-
-
-
-			for (i = 0; i < n; i++)
-
-					{
-
-								s[i] = b;
-
-									}
-
-				return (s);
-
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
 
-
-
 /**
+ *_calloc - allocates memory for an array using malloc
  *
- *  *_calloc - allocates memory for an array using malloc
+ *@nmemb: the amount of spaces that needs to be allocated
+ *@size: size of each of the allocated spaces
  *
- *   *
- *
- *    *@nmemb: the amount of spaces that needs to be allocated
- *
- *     *@size: size of each of the allocated spaces
- *
- *      *
- *
- *       *Return: a pointer to the allocated array or NULL if the process fails
- *
- *        */
-
+ *Return: a pointer to the allocated array or NULL if the process fails
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
+	char *array;
 
-		char *array;
-
-
-
-			if (nmemb == 0 || size == 0)
-
-					{
-
-								return (NULL);
-
-									}
-
-				array = malloc(nmemb * size);
-
-					if (array == NULL)
-
-							{
-
-										return (NULL);
-
-													}
-
-						array = _memset(array, 0, nmemb * size);
-
-							return (array);
-
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(nmemb * size);
+	if (array == NULL)
+	{
+		return (NULL);
+			}
+	array = _memset(array, 0, nmemb * size);
+	return (array);
 }
