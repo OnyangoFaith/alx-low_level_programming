9fc63d82ae98bdc34f3db066a8ef9147149be6ed
OnyangoFaith <117824136+OnyangoFaith@users.noreply.github.com>
2023-01-21 01:44:12 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,135 +1,29 @@
 #include "3-calc.h"
 
-
-
 /**
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the use
+ * @s: char operator
  *
- *  * op_add - adds two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: add
- *
- *       */
-
-int op_add(int a, int b)
-
+ * Return: pointer to the function that corresponds to the operator
+ */
+int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-		return (a + b);
-
-}
-
-/**
- *
- *  * op_sub - subtracts two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: difference
- *
- *       */
-
-int op_sub(int a, int b)
-
-{
-
-		return (a - b);
-
-}
-
-/**
- *
- *  * op_mul - multiplies two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: multiples
- *
- *       */
-
-int op_mul(int a, int b)
-
-{
-
-		return (a * b);
-
-}
-
-/**
- *
- *  * op_div - divides two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: division
- *
- *       */
-
-int op_div(int a, int b)
-
-{
-
-		if (b == 0)
-
-				{
-
-							printf("Error\n");
-
-									exit(100);
-
-										}
-
-			return (a / b);
-
-}
-
-
-
-/**
- *
- *  * op_mod - calculates the module of two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: remainder of the division
- *
- *       */
-
-int op_mod(int a, int b)
-
-{
-
-		if (b == 0)
-
-				{
-
-							printf("Error\n");
-
-									exit(100);
-
-										}
-
-			return (a % b);
-
+	while (i < 10)
+	{
+		if (s[0] == ops->op[i])
+			break;
+		i++;
+	}
+	return (ops[i / 2].f);
 }
