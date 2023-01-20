3f57b43c6f492b99113d9ba9926cd038bc7bbec8
OnyangoFaith <117824136+OnyangoFaith@users.noreply.github.com>
2023-01-21 01:47:43 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,135 +1,68 @@
 #include "3-calc.h"
 
-
-
 /**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
  *
- *  * op_add - adds two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: add
- *
- *       */
-
+ * Return: add
+ */
 int op_add(int a, int b)
-
 {
-
-		return (a + b);
-
+	return (a + b);
 }
-
 /**
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
  *
- *  * op_sub - subtracts two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: difference
- *
- *       */
-
+ * Return: difference
+ */
 int op_sub(int a, int b)
-
 {
-
-		return (a - b);
-
+	return (a - b);
 }
-
 /**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
  *
- *  * op_mul - multiplies two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: multiples
- *
- *       */
-
+ * Return: multiples
+ */
 int op_mul(int a, int b)
-
 {
-
-		return (a * b);
-
+	return (a * b);
 }
-
 /**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
  *
- *  * op_div - divides two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: division
- *
- *       */
-
+ * Return: division
+ */
 int op_div(int a, int b)
-
 {
-
-		if (b == 0)
-
-				{
-
-							printf("Error\n");
-
-									exit(100);
-
-										}
-
-			return (a / b);
-
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
-
-
 /**
+ * op_mod - calculates the module of two numbers
+ * @a: first number
+ * @b: second number
  *
- *  * op_mod - calculates the module of two numbers
- *
- *   * @a: first number
- *
- *    * @b: second number
- *
- *     *
- *
- *      * Return: remainder of the division
- *
- *       */
-
+ * Return: remainder of the division
+ */
 int op_mod(int a, int b)
-
 {
-
-		if (b == 0)
-
-				{
-
-							printf("Error\n");
-
-									exit(100);
-
-										}
-
-			return (a % b);
-
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
