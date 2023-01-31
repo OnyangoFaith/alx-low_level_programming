2640ab000bc1c84824c4de9c9e1f1818a77eac7b
OnyangoFaith <117824136+OnyangoFaith@users.noreply.github.com>
2023-02-01 01:02:39 +0300

    Add files via upload

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * print_list - function that prints all elements of a list
-  * @h: linked list
-  * Return: number of nodes
-  */
-
+ * print_list - a function that prints all the elements of a list_t list
+ * @h: list
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t l_node;
 
+	l_node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		h = h->next;
-		count++;
+		l_node++;
 	}
-
-	return (count);
+	return (l_node);
 }
-
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,22 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
-  * list_len - function that returns the number of elements in a linked list
-  * @h: linkede list
-  * Return: number of elements
-  */
-
+ * list_len - a function that returns the number of elements in
+ * a linked list_t list.
+ *
+ * @h: list
+ * Return:  number of elements in a linked list_t
+ */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
 
-	while (h)
+	size_t length;
+
+	length = 0;
+	while (h != NULL)
 	{
 		h = h->next;
-		count++;
+		length++;
 	}
-
-	return (count);
+	return (length);
 }
